103c7a00bcb75dad4343ba92a1f8957d3fc5c78f
Hasanat902 <abulhasanat902@gmail.com>
2022-02-12 11:59:56 +0600

    withdraw button js event added

--- js/index.js
+++ js/index.js
@@ -22,3 +22,28 @@ document.getElementById("deposit-button").addEventListener("click", function(){
     //clear deposit amount
     depositInput.value = "";
 });
+
+// handle withdraw button event
+document.getElementById("withdraw-button").addEventListener("click", function(){
+    
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawAmountText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
+
+    withdrawTotal.innerText = previousWithdrawAmount + withdrawAmount;
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalance = parseFloat(previousBalanceText);
+
+    balanceTotal.innerText = previousBalance - withdrawAmount;
+
+
+    //clear withdraw amount
+    withdrawInput.value = "";
+})
